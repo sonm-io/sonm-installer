80348eef40012a85521bcc74e86fb36b024fcb40
maestrow <andrew.shmelev@gmail.com>
2018-09-26 15:54:11 +0300

    restructure solution

--- SonmInstaller.ViewModel/Script.fsx
+++
@@ -1,8 +0,0 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
-
-#load "UiState.fs"
-open SonmInstaller.ViewModel
-
-// Define your library scripting code here
-
